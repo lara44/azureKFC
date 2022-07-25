8d2fc578eec46945a31b18acf3bc422659ca88db
lara44 <adames.lancero@gmail.com>
2022-07-25 08:39:00 -0500

    add entities

---
+++ azueKFC.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace azueKFC.Common.Models
+{
+    class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public string IsCompleted { get; set; }
+    }
+}
---
+++ azureKFC.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace azureKFC.Functions.Entities
+{
+    class TodoEntity: TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public string IsCompleted { get; set; }
+    }
+}
+}
